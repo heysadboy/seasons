449904cca30f8889eec35aa410c734914cb09c02
heysadboy <heysadboy@gmail.com>
2021-01-05 02:05:47 +0900

    able to display screen on the basis of seasons now

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = { lat: null, errorMessage: '' };
-
-    }
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
@@ -23,7 +19,11 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return (
+                <div>
+                    <SeasonDisplay lat={this.state.lat} />
+                </div>
+            );
         }
 
         return <div>Loading!</div>;
